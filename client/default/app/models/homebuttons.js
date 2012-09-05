825edf1563565a269f868dd7b4637066ba1b264a
emanouil.kazaltzis@mc2bis.eu <emanouil.kazaltzis@mc2bis.eu>
2012-09-05 14:01:55 +0000

    update file /client/default/app/models/homebuttons.js

--- client/default/app/models/homebuttons.js
+++ client/default/app/models/homebuttons.js
@@ -15,7 +15,18 @@ app.stores.home = new Ext.data.Store({
 			              controller: app.controllers.camera, action: 'openCamera'
 			          });
 		  		     }
-		  	     }
+		  	     },
+        },
+        { button:
+       	     {
+		  		      xtype: 'button',
+		  		      cls: 'cameraIcon',
+		  		      handler: function() {
+		  		      Ext.dispatch({
+			              controller: app.controllers.camera, action: 'openCamera'
+			          });
+		  		     }
+		  	     },
         }
       ]
 });
