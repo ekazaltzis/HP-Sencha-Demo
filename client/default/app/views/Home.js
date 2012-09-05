80133e0daf722395b5e111fe27e522f7e3e997aa
emanouil.kazaltzis@mc2bis.eu <emanouil.kazaltzis@mc2bis.eu>
2012-09-05 13:46:04 +0000

    update file /client/default/app/views/Home.js

--- client/default/app/views/Home.js
+++ client/default/app/views/Home.js
@@ -12,53 +12,6 @@ layout: {
   		title: '<img class="logo" src="app/images/logo.png" />',
   	}
   ],
+  store : apps.store.HomeButtons
 
-  items: [
-    {
-  	  		xtype: 'button',
-		  		cls: 'mapIcon',
-		  		handler: function() {
-		  			app.views.viewport.setActiveItem(app.views.map, {type: 'slide', direction: 'left'});
-		  		}      
-    },
-    
-  	  	{
-		  		xtype: 'button',
-		  		cls: 'twitterIcon',
-		  		handler: function() {
-		  			app.views.viewport.setActiveItem(app.views.twitter, {type: 'slide', direction: 'left'});
-		  		}
-		  	},
-		  	{
-		  		xtype: 'button',
-		  		cls:  'paymentIcon',
-		  		handler: function() {
-		  			app.views.viewport.setActiveItem(app.views.payment, {type: 'slide', direction: 'left'});
-		  		}
-		  	},
-		  	{
-		  		xtype: 'button',
-		  		cls: 'cameraIcon',
-		  		handler: function() {
-		  		  Ext.dispatch({
-			        controller: app.controllers.camera,
-			        action: 'openCamera'
-			      });
-		  		}
-		  	},
-		  	{
-		  		xtype: 'button',
-		  		cls: 'webviewIcon',
-		  		handler: function() {
-		  			app.views.viewport.setActiveItem(app.views.stocks, {type: 'slide', direction: 'left'});
-		  		}
-		  	},
-		  	{
-		  		xtype: 'button',
-		  		cls: 'settingsIcon',
-		  		handler: function() {
-		  			app.views.viewport.setActiveItem(app.views.settings, {type: 'slide', direction: 'left'});
-		  		}
-		  	}
-  ]
 });
