32d356b5f4a3ab947ef32216e3214bcd65a6bc7f
emanouil.kazaltzis@mc2bis.eu <emanouil.kazaltzis@mc2bis.eu>
2012-09-05 13:44:31 +0000

    update file /client/default/app/models/homebuttons.js

--- client/default/app/models/homebuttons.js
+++ client/default/app/models/homebuttons.js
@@ -1,8 +1,30 @@
-app.models.HomeButtons = Ext.regModel('app.models.Stocks', {
-    fields: ['Name', 'Symbol', 'Last', 'Change', 'Date', 'Time', 'Open'],
+app.models.HomeButtons = Ext.regModel('app.models.HomeButtons', {
+    fields: ['button'],
 });
 
 app.stores.stocks = new Ext.data.Store({
   model: 'app.models.Stocks',
-    data : []
+    data : 
+      [
+        { button:
+   	  	     {
+		  		      xtype: 'button',
+		  		      cls: 'cameraIcon',
+		  		      handler: function() {
+		  		      Ext.dispatch({
+			              controller: app.controllers.camera, action: 'openCamera'
+			          });
+		  		   }
+		  	},
+        { button:
+       	     {
+		  		      xtype: 'button',
+		  		      cls: 'cameraIcon',
+		  		      handler: function() {
+		  		      Ext.dispatch({
+			              controller: app.controllers.camera, action: 'openCamera'
+			          });
+		  		   }
+		  	}
+      ]
 });
